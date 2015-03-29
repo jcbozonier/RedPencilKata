80c3afd14769549458ff330f6377479ac7167033
Justin Bozonier <darkxanthos@gmail.com>
2015-03-29 09:39:53 -0500

    First domain related test
    Starting off with a simple function definition in the same file as the
    class. Just writing the simplest function possible passed the test.

--- tests/redline_tests.py
+++ tests/redline_tests.py
@@ -1,2 +1,14 @@
+import unittest
+
 def always_passing_test():
   assert True
+
+def price_changed(from_price, to_price):
+    pass
+
+class given_a_good_with_no_price_change_ever(unittest.TestCase):
+    def setUp(self):
+        self.original_good_price = 100.00
+    def test_when_price_is_changed_to_same_price(self):
+        red_line_promotion_started = price_changed(from_price=100.00, to_price=100.00)
+        self.assertFalse(red_line_promotion_started, "It should not start a red line promotion.")
