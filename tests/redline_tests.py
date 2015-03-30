602802e75354fc8ff9928ebd6f020657ceb9f7be
Justin Bozonier <darkxanthos@gmail.com>
2015-03-29 19:34:44 -0500

    Refactoring for clearer tests
    Noticed I had gotten into a groove and hadn’t been using my set up
    variable. Fixed that and tried to make it clearer how the different
    percentages tie into the expected behavior of the test.

--- tests/redline_tests.py
+++ tests/redline_tests.py
@@ -8,14 +8,18 @@ class given_a_good_with_no_price_change_ever(unittest.TestCase):
     def setUp(self):
         self.original_good_price = 100.00
     def test_when_price_is_changed_to_same_price(self):
-        red_line_promotion_started = price_changed(from_price=100.00, to_price=100.00)
+        red_line_promotion_started = price_changed(from_price=self.original_good_price,
+                                                   to_price=self.original_good_price)
         self.assertFalse(red_line_promotion_started, "It should not start a red line promotion.")
     def test_when_price_is_reduced_by_5_percent(self):
-        red_line_promotion_started = price_changed(from_price=100.00, to_price=95.00)
+        red_line_promotion_started = price_changed(from_price=self.original_good_price,
+                                                   to_price=(1 - 0.05) * self.original_good_price)
         self.assertTrue(red_line_promotion_started, "It should start a red line promotion.")
     def test_when_price_is_reduced_by_30_percent(self):
-        red_line_promotion_started = price_changed(from_price=100.00, to_price=70.00)
+        red_line_promotion_started = price_changed(from_price=self.original_good_price,
+                                                   to_price=(1 - 0.30) * self.original_good_price)
         self.assertTrue(red_line_promotion_started, "It should start a red line promotion.")
     def test_when_price_is_reduced_by_5_percent_to_30_percent(self):
-        red_line_promotion_started = price_changed(from_price=100.00, to_price=90.00)
+        red_line_promotion_started = price_changed(from_price=self.original_good_price,
+                                                   to_price=(1 - 0.10) * self.original_good_price)
         self.assertTrue(red_line_promotion_started, "It should start a red line promotion.")
