f4cbbb627e68ab5fdef6a93c15b9ee2f58a01fc5
Justin Bozonier <darkxanthos@gmail.com>
2015-03-29 19:45:10 -0500

    Adding handling for previous price changes

--- red_line_promotion.py
+++ red_line_promotion.py
@@ -1,2 +1,6 @@
-def price_changed(from_price, to_price):
-    return to_price/from_price <= 0.95 and to_price/from_price >= 0.7
+import datetime
+
+def price_changed(from_price, to_price, todays_date=datetime.datetime.now(), last_price_change_date=datetime.datetime.min):
+    time_delta_since_last_price_change = todays_date - last_price_change_date
+    if time_delta_since_last_price_change.days > 30:
+        return to_price/from_price <= 0.95 and to_price/from_price >= 0.7
--- tests/redline_tests.py
+++ tests/redline_tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, datetime
 from red_line_promotion import price_changed
 
 def always_passing_test():
@@ -23,3 +23,15 @@ class given_a_good_with_no_price_change_ever(unittest.TestCase):
         red_line_promotion_started = price_changed(from_price=self.original_good_price,
                                                    to_price=(1 - 0.10) * self.original_good_price)
         self.assertTrue(red_line_promotion_started, "It should start a red line promotion.")
+
+class given_a_good_with_a_price_change_in_the_last_thirty_days(unittest.TestCase):
+    def setUp(self):
+        self.last_price_change_date = datetime.datetime.strptime('2015-03-01 17:59:59', '%Y-%m-%d %H:%M:%S')
+        self.todays_date = datetime.datetime.strptime('2015-03-29 12:14:12', '%Y-%m-%d %H:%M:%S')
+        self.original_good_price = 200.00
+    def test_when_price_is_reduced_by_5_percent(self):
+        red_line_promotion_started = price_changed(from_price = self.original_good_price,
+                                                   to_price = (1 - 0.05) * self.original_good_price,
+                                                   todays_date = self.todays_date,
+                                                   last_price_change_date=self.last_price_change_date)
+        self.assertFalse(red_line_promotion_started, "It should NOT start a red line promotion.")
