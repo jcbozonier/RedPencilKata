31c4b0c5f4f04b0cbadb300674bfd1ac497a336c
Justin Bozonier <darkxanthos@gmail.com>
2015-03-29 21:47:40 -0500

    Last refactoring that gets us on our way
    Now we can get busy on the next test.

--- tests/redline_tests.py
+++ tests/redline_tests.py
@@ -31,14 +31,11 @@ class given_a_good_with_a_price_change_in_the_last_thirty_days(unittest.TestCase
 
 class given_a_good_with_a_price_change_before_the_last_thirty_days(unittest.TestCase):
     def setUp(self):
-        self.last_price_change_date = datetime.datetime.strptime('2015-02-26 11:59:59', '%Y-%m-%d %H:%M:%S')
         self.todays_date = datetime.datetime.strptime('2015-03-29 12:14:12', '%Y-%m-%d %H:%M:%S')
-        self.original_good_price = 200.00
+        self.good = Good(price=200.00)
+        self.good.reduce_price(by=0.05, effective=datetime.datetime.strptime('2015-02-26 11:59:59', '%Y-%m-%d %H:%M:%S'))
     def test_when_price_is_reduced_by_5_percent(self):
-        red_line_promotion_started = price_changed(from_price = self.original_good_price,
-                                                   to_price = reduce_price_of(self.original_good_price, by=0.05),
-                                                   todays_date = self.todays_date,
-                                                   last_price_change_date=self.last_price_change_date)
+        red_line_promotion_started = self.good.reduce_price(by=0.05, effective=self.todays_date)
         self.assertTrue(red_line_promotion_started, "It should start a red line promotion.")
 
 class given_a_good_price(unittest.TestCase):
